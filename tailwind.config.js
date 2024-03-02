17d2be86f953ee187895e59ce1583e8323e18d4b
Sarajit-mondal <bdsarajit499@gmail.com>
2024-03-02 18:57:15 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "primary-color": "#1DD100",
+      "drak-color": "#030712",
+      "light-color": "#FFFFFF",
+    },
+    fontFamily: {
+      // raliway: ["Raleway", "sans-serif"],
+      // roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
