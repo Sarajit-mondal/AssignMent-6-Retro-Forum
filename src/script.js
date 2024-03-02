3b49b8b3b5696bddbf3cdb5ad2a441405e134703
Sarajit-mondal <bdsarajit499@gmail.com>
2024-03-02 21:55:54 +0600

    nav bar complete

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("dsfsadf");
+// API
+// LatestPosts: - https://openapi.programming-hero.com/api/retro-forum/latest-posts
+
+// AllPosts: - https://openapi.programming-hero.com/api/retro-forum/posts
+
+// PostSearchByQuery
+// PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?category=categoryName
+
+// Example
+// PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?category=coding
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      "primary-color": "#1DD100",
       "drak-color": "#030712",
       "light-color": "#FFFFFF",
+      "gray-color": "#808080",
     },
     fontFamily: {
       // raliway: ["Raleway", "sans-serif"],
       // roboto: ["Roboto", "sans-serif"],
+      Mulish: ["Mulish", "sans-serif"],
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
